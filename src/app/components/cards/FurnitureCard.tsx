7fe4af694d6c400b086ceee88df32b3cc541f40e
nFd_noFugaziDev <mubaraqmuhammad06@gmail.com>
2024-03-11 16:08:45 +0100

    implement the Furniture Card Props

--- src/app/components/cards/FurnitureCard.tsx
+++ src/app/components/cards/FurnitureCard.tsx
@@ -1,7 +1,35 @@
-export default function FunitureCard() {
-    return (
-        <div>
-            
-        </div>
-    )
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface FurnitureCardProps {
+  name: string;
+  image: string;
+  price?: number;
+  buy: string;
 }
+
+const FurnitureCard: React.FC<FurnitureCardProps> = ({
+  name,
+  image,
+  price,
+  buy,
+}) => {
+  return (
+    <div className="card p-10 bg-custom-gray w-fit">
+      <div>
+        <Image src={image} alt="image" width={300} height={300} />
+        <p className="pt-5 uppercase sm:text-base lg:text-2xl">{name}</p>
+
+        <div className="btm flex items-center justify-between pt-7">
+          <p><span className="text-custom-green">$</span>{price}</p>
+          <p>
+            <Link href='/' className="text-custom-green uppercase">{buy}</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FurnitureCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import FurnitureCard from '../app/components/cards/FurnitureCard'
 import './styles.css'
+
 
 export default function Home() {
   return (
-    <main className="main-body">
-        
+    <main className="main-body p-10">
+        <FurnitureCard name='flat chair' price={40} buy='buy now' image="/dd.png" />
     </main>
   );
 }
