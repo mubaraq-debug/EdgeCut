edeff9d04b0674fffbcfe13b3e3c77de26658ad0
nFd_noFugaziDev <mubaraqmuhammad06@gmail.com>
2024-03-16 19:47:02 +0100

    chore: update hero section

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -4,15 +4,21 @@ const HeroSection = () => {
   return (
     <div className="hero-section bg-custom-gray md:flex md:flex-row items-center p-16 justify-around sm:flex sm:flex-col sm:gap-16">
       <div className="hero-text">
-        <h3 className="uppercase text-5xl font-bold pb-10">For all your furniture needs</h3>
+        <h3 className="uppercase lg:text-5xl md:text-3xl sm:text-2xl font-bold pb-10">
+          For all your furniture needs
+        </h3>
         <p className="text-justify pb-10">
           Enter our world of luxury furniture, where elegance meets comfort in
           every meticulously crafted piece. Explore our hero section to discover
           the epitome of sophistication and timeless beauty for your home.
         </p>
         <div className="btns flex gap-5">
-          <button className="bg-custom-orange text-white px-8 py-3 cursor-pointer hover:bg-transparent hover:border hover:border-custom-orange hover:text-custom-orange capitalize">contact us</button>
-          <button className="bg-custom-green text-white px-8 py-3 cursor-pointer hover:bg-transparent hover:border hover:border-custom-green hover:text-custom-green capitalize">about us</button>
+          <button className="bg-custom-orange text-white px-5 py-3 cursor-pointer hover:bg-transparent hover:border hover:border-custom-orange hover:text-custom-orange capitalize">
+            contact us
+          </button>
+          <button className="bg-custom-green text-white px-5 py-3 cursor-pointer hover:bg-transparent hover:border hover:border-custom-green hover:text-custom-green capitalize">
+            about us
+          </button>
         </div>
       </div>
       <div className="hero-image">
